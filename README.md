f6ce0250965956173e28435739bef83935d7a17a
Alvaro Maraver Lerma <alvmarler@alum.us.es>
2023-02-21 22:56:50 +0100

    faltan los 3 algoritmos

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # SOS2223-12
 
 Sesión de Feedback 03 en proceso.
+Carpeta node_modules en linea 41 del .gitignore que está comentada.
---
+++ index-YYY.js
@@ -0,0 +1,10 @@
+// Prueba sin la carpeta node_modules
+var a = 1;
+console.log(a);
+
+//Hacer algoritmo:
+// Álvaro
+
+// Jorge
+
+// Víctor
