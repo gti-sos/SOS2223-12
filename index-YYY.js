7aea98f678c779ed67b961c2843c3da7d24a76ad
Alvaro Maraver Lerma <alvmarler@alum.us.es>
2023-02-22 00:16:50 +0100

    algoritmo alvaro terminado

--- index-YYY.js
+++ index-YYY.js
@@ -1,9 +1,24 @@
-// Prueba sin la carpeta node_modules
-var a = 1;
-console.log(a);
+//Hacer los algoritmos:
+// Álvaro: temperatura media de Sevilla en 10 días.
+var lista = [27.57, 27.19, 27.57, 26.11, 26.05, 27.23, 25.52, 27.19, 27.72, 27.76];
 
-//Hacer algoritmo:
-// Álvaro
+// Prueba 1.
+var resultado1 = 0;
+
+lista.forEach(function(a){
+    resultado1 += a;
+});
+console.log("Prueba 1 -> La temperatura media de Sevilla en 10 días es: " +resultado1/10);
+
+//Prueba 2.
+var resultado2 = 0;
+var i = 0;
+function b(){
+    resultado2 += lista[i];
+    i++;
+}
+lista.forEach(b);
+console.log("Prueba 2 -> La temperatura media de Sevilla en 10 días es: " +resultado2/10);
 
 // Jorge
 
