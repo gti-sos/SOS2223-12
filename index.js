a738ca01bf4c50d852b4ac2b56fcdca885dd6fe8
Alvaro Maraver Lerma <alvmarler@alum.us.es>
2023-02-28 19:42:51 +0100

    caritas

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 // ruta dinámica /cool
+var cool = require("cool-ascii-faces");
+var express = require("express");
 
+var app = express();
+var port = 12345
 
+app.get("/faces", (request, response) => {
+    response.send(cool());
+    console.log("Nueva carita");
+});
+
+app.listen(port);
+console.log("Servidor funcionando");
+
+/*
+Escribe "Servidor funcionando" cuando realmente el servidor está escuchando, 
+el anterior lo escribe un poco antes de que empiece a funcionar
+app.listen(port, () => {
+    console.log(`Servidor funcionando ${port}`);
+})
+*/
 
 // ruta del algoritmo de Álvaro
 
