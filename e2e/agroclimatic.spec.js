0328b5143880f7f3006780c594dee6e7cad5f290
jorfigrom <jf26012@gmail.com>
2023-05-24 19:13:03 +0200

    a

--- e2e/agroclimatic.spec.js
+++ e2e/agroclimatic.spec.js
@@ -18,6 +18,7 @@ test('navigate to agroclimatic page', async ({ page }) => {
     await expect(page).toHaveTitle("SOS2223-12-Datos-Agroclimáticas");
 
     await page.waitForTimeout(1000);
+    
 
     // Comprobación de que hay datos
     await expect((await page.locator(".datosAgro").all()).length).toBeGreaterThan(0);
